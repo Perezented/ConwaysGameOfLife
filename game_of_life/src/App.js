756cc14cfbbcbe016d144605308c0dad781bee93
Michael Perez <mperez6473@gmail.com>
2020-07-27 12:05:42 -0500

    addition of grid component

--- game_of_life/src/App.js
+++ game_of_life/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-function App() {
-    return <div className="App"></div>;
+import Grid from "./components/Grid";
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            generation: 0,
+        };
+    }
+    render() {
+        return (
+            <div>
+                <h1>The Game of Life</h1>
+                <Grid />
+                <h2>Generations:{this.state.generation}</h2>
+            </div>
+        );
+    }
 }
 
 export default App;
