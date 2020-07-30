e094466ef6dfceae23f136ccae6178feae768927
Michael Perez <mperez6473@gmail.com>
2020-07-30 11:08:28 -0500

    Gridtemplates did not send

--- game_of_life/src/components/GridTemplates.js
+++ game_of_life/src/components/GridTemplates.js
@@ -5,6 +5,7 @@ export default function GridTemplates(props) {
         <div className="buttons">
             <button onClick={props.bigX}>BigX</button>
             <button onClick={props.seed}>Randomize</button>
+
             <button onClick={props.clear}>Clear</button>
         </div>
     );
