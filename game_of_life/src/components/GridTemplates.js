206366a7d9125d50c4481f12410add8ef0824fc6
Michael Perez <mperez6473@gmail.com>
2020-07-30 11:06:12 -0500

    GridTemplates sending now

---
+++ game_of_life/src/components/GridTemplates.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function GridTemplates(props) {
+    return (
+        <div className="buttons">
+            <button onClick={props.bigX}>BigX</button>
+            <button onClick={props.seed}>Randomize</button>
+            <button onClick={props.clear}>Clear</button>
+        </div>
+    );
+}
